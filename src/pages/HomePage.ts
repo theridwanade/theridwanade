d001443f29410643361a3a9fb06d430a8949a1cd
theridwanade <theridwanade@gmail.com>
2026-02-12 21:34:50 +0100

    Upgraded site to me more natural and direct

--- src/pages/HomePage.ts
+++ src/pages/HomePage.ts
@@ -1,30 +1,49 @@
 export const HomePage = () => {
   return (`
     <div class="main">
-    <div class="hs-image-main">
-    <img class="hs-image" src="/theridwanade.jpg" alt="Ridwan" />
-    </div>
+      <div class="hs-image-main">
+        <img class="hs-image" src="/theridwanade.jpg" alt="Ridwan" />
+      </div>
 
-      <p class="h1">Hi, I'm Ridwan.</p>
+      <h1>Hi, I'm Ridwan.</h1>
 
       <p>
-        I'm a programmer studying Library and Information Science at the University of Ilorin. I build intresting softwares.
+        I'm a programmer and computer science enthusiast. I build open-source projects and software that educate and showcase the marvels of computer science.
       </p>
 
       <p>
-        See my projects and contributions on <a href="https://github.com/theridwanade" target="_blank">GitHub</a>.
+        Check out my projects and contributions on 
+        <a href="https://github.com/theridwanade" target="_blank">GitHub</a>.
       </p>
 
       <p>
-        Read my technical articles on <a href="https://dev.to/theridwanade" target="_blank">dev.to</a> and my thoughts on topics I find interesting on <a href="https://medium.com/@theridwanade" target="_blank">Medium</a>.
+        I am currently pursuing my degree in Library and Information Science at the University of Ilorin, where I research information management and dissemination. I aim to combine my computer science skills and academic knowledge to create innovative solutions that benefit society.
       </p>
 
       <p>
-        Connect or reach out on <a href="https://linkedin.com/in/theridwanade" target="_blank">LinkedIn</a> or via <a href="mailto:ridwan@theridwanade.me">email</a>.
+        Read my articles on science, society, technology, and other topics I find interesting on 
+        <a href="https://medium.com/@theridwanade" target="_blank">Medium</a>.
       </p>
 
       <p>
-        Subscribe to my <a href="https://theridwanade.substack.com" target="_blank">newsletter</a> for updates on projects and writing.
+        You can also read my technical articles on 
+        <a href="https://dev.to/theridwanade" target="_blank">dev.to</a>.
+      </p>
+
+      <p>
+        Follow my thoughts and updates on X (formerly Twitter) at 
+        <a href="https://twitter.com/theridwanade" target="_blank">@theridwanade</a>.
+      </p>
+
+      <p>
+        Connect with me professionally on 
+        <a href="https://linkedin.com/in/theridwanade" target="_blank">LinkedIn</a>, or reach out via 
+        <a href="mailto:ridwan@theridwanade.me">email</a> for projects, research, events, or just to say hello.
+      </p>
+
+      <p>
+        To stay updated on my projects, articles, and other work, subscribe to my 
+        <a href="https://theridwanade.substack.com" target="_blank">newsletter</a> to get emails whenever I publish something new.
       </p>
 
       <iframe
@@ -35,6 +54,17 @@ export const HomePage = () => {
         frameborder="0"
         scrolling="no"
       ></iframe>
+
+      <footer style="margin-top:40px; padding:20px 0; text-align:center; border-top:1px solid #EEE; color:#555; font-size:14px;">
+        <p>© ${new Date().getFullYear()} Ridwan Ade. All rights reserved.</p>
+        <p>
+          Follow me on 
+          <a href="https://github.com/theridwanade" target="_blank">GitHub</a>, 
+          <a href="https://linkedin.com/in/theridwanade" target="_blank">LinkedIn</a>, 
+          <a href="https://medium.com/@theridwanade" target="_blank">Medium</a>, 
+          <a href="https://twitter.com/theridwanade" target="_blank">X (Twitter)</a>
+        </p>
+      </footer>
     </div>
-  `)
+  `);
 }
